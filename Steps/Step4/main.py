7199a726e70f85d748c48705491d88d62fa0a698
bonhou_q <bonhou_q@etna-alternance.net>
2022-02-07 15:22:31 +0100

    Suppression steps

--- Steps/Step4/main.py
+++
@@ -1,7 +0,0 @@
-def shortcut(s):
-    characters = "aeiou"
-    word = ""
-    for i in s:
-        if not i in characters:
-            word += i
-    return word
--- Steps/Step5/main.py
+++
@@ -1,5 +0,0 @@
-def square_digits(num):
-    res = ""
-    for i in range(len(str(num))):
-        res = res + str(int(str(num)[i]) ** 2)
-    return int(res)
